554759afc71267f03790dd4f1de77750aa51e0ac
acidburnmonkey <acidburnmonkey@gmail.com>
2025-05-25 20:58:50 -0400

    uv dependencies

--- preinstall.sh
+++ preinstall.sh
@@ -4,7 +4,7 @@
 sudo dnf check-update
 
 # DNF reqs
-sudo dnf install -y python3-tkinter python3-devel python3-pip
+sudo dnf install -y python3-tkinter python3-devel python3-pip uv
 
 #pip
 python3 -m pip3 install --upgrade pip
